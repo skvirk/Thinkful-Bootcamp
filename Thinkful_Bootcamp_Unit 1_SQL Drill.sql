3dff0efe6c43ec70959b19abb334b261679fd6fc
skvirk <38390021+skvirk@users.noreply.github.com>
2018-05-11 13:22:35 -0400

    Bootcamp_Unit 1_SQL Drill 1

---
+++ Thinkful_Bootcamp_Unit 1_SQL Drill.sql
@@ -0,0 +1,21 @@
+SELECT 
+	trip_id
+FROM 
+	trips
+WHERE 
+	bike_id > 500
+ORDER BY duration
+
+SELECT 
+	*
+FROM 
+	stations
+WHERE 
+	station_id = '84'
+	
+SELECT 
+	min_temp
+FROM 
+	Occurrences Rain
+WHERE 
+	zip_code = '94301'
